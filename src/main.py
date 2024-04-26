7737f5e00ae63ec27d440837b49ed30a703dfd9d
Daniel Petkau <dpetkau@uw.edu>
2024-04-26 16:44:12 -0700

    Change api key

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-api_key = "sk-proj-s6qk8TOxraaftjlTJPGPT3BlbkFJj1ygF0i7FlmY5pSR7yB8"
+from openai import OpenAI # type: ignore
+
+api_key = "sk-proj-uKBt9syTrOBcCtkW107AT3BlbkFJ5veseUervdY2Y6Z1IWh1"
+client = OpenAI(api_key=api_key)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion.choices[0].message.content)
