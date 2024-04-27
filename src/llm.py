c9e640ab5545a5d1d0011a46dacb6c80757a3603
Daniel Petkau <dpetkau@uw.edu>
2024-04-26 17:30:14 -0700

    Add create_response()

---
+++ src/llm.py
@@ -0,0 +1,16 @@
+from openai import OpenAI # type: ignore
+
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+def create_response(message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message,
+            },
+        ],
+        model="phi3",
+    )
+
+    return chat_completion.choices[0].message.content
