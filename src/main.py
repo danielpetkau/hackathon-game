64f5b2904c68535abf10023fa094279699f13ff2
Daniel Petkau <dpetkau@uw.edu>
2024-04-26 17:17:09 -0700

    Switch to LMstudio

--- src/main.py
+++ src/main.py
@@ -1,16 +1,32 @@
 from openai import OpenAI # type: ignore
 
-api_key = "sk-proj-uKBt9syTrOBcCtkW107AT3BlbkFJ5veseUervdY2Y6Z1IWh1"
-client = OpenAI(api_key=api_key)
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
             "content": "Say this is a test",
-        }
+        },
+        # {
+        #     "role": "system",
+        #     "content": "Speak in rhymes",
+        # }
     ],
-    model="gpt-3.5-turbo",
+    model="phi3",
 )
-
+    
 print(chat_completion.choices[0].message.content)
+
+def create_response(message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message,
+            },
+        ],
+        model="phi3",
+    )
+
+    return chat_completion.choices[0].message.content
